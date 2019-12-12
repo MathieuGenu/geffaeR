32c2339109aa441ce04b708c021ca9d03ec788af
mgenu <mathieu.genu@univ-lr.fr>
2019-12-12 15:41:32 +0100

    - avoid warning message at the beginning of package loading because of conflicts between functions

--- R/import_package.R
+++ R/import_package.R
@@ -1,6 +1,9 @@
-#' @import rgdal maptools sp raster foreign reshape FactoMineR missMDA
+#' @import rgdal maptools sp foreign FactoMineR missMDA
 #' @import dplyr ggplot2 cowplot mvtnorm Distance captioner Amelia
-#' @import Rdistance dsm knitr fields ggthemes broom coda
+#' @import Rdistance dsm knitr fields broom coda
+#' @rawNamespace import(ggthemes, except = theme_map)
+#' @rawNamespace import(raster, except = c(intersect, union, select))
+#' @rawNamespace import(reshape, except = c(stamp, rename))
 # @import methods Rcpp
 #' @importFrom stats integrate rnorm sd as.formula coef cor diffinv glm
 #' poisson rgamma rnbinom rpois var vcov
