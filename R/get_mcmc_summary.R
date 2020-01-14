e1ae5cee1911cc934254d79964ad23f34500de92
mgenu <mathieu.genu@univ-lr.fr>
2020-01-14 09:43:10 +0100

    create get_mcmc_summary function

---
+++ R/get_mcmc_summary.R
@@ -0,0 +1,28 @@
+#' @export
+
+get_mcmc_summary <- function(x, alpha = 0.2, dig = 4, median = FALSE) {
+
+  if(!coda::is.mcmc(x)) {
+    x <- coda::as.mcmc(x)
+  }
+
+  if(median) {
+    summary <- round(c(median(x), mad(x), mad(x) / median(x), coda::HPDinterval(x, prob = 1 - alpha)), dig)
+    summary <- data.frame(summary)
+    row_name_one_time <- c("median","mad","mad/meadian",
+                           paste("HPDlower at ",(1-alpha),sep=""),
+                           paste("HPDupper at ",(1-alpha),sep=""))
+    row.names(summary) <- rep(row_name_one_time)
+
+  } else {
+
+    summary <- round(c(mean(x), sd(x), sd(x) / mean(x), coda::HPDinterval(x, prob = 1 - alpha)), dig)
+    summary <- data.frame(summary)
+    row.names(summary) <- c("mean","sd","sd/mean",
+                            paste("HPDlower at ",(1-alpha),sep=""),
+                            paste("HPDupper at ",(1-alpha),sep=""))
+  }
+
+  return(summary)
+
+}
