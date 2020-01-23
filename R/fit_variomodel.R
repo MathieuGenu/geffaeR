00bbf61de2975fb6cf024c271ff4c9d885fb4d3a
mgenu <mathieu.genu@univ-lr.fr>
2020-01-23 10:37:04 +0100

    sort fucntion dans krigeage

---
+++ R/fit_variomodel.R
@@ -0,0 +1,20 @@
+
+#' @export
+
+# fitter le model "matern" ou "exponential", qui sert pour les autres fonctions
+fit_variomodel <- function(variogram, form = c("matern", "exponential")) {
+  vario_model <- geoR::variofit(variogram,
+                                cov.model = form,
+                                fix.nugget = FALSE,
+                                nugget = mean(variogram$v) / 4,
+                                fix.kappa = TRUE, kappa = ifelse(form == "matern", 1.5, 1),
+                                weights = "npairs",
+                                minimisation.function = "optim",
+                                ini.cov.pars = expand.grid(
+                                  seq(0, mean(variogram$v)*2, l = 10),
+                                  seq(as.numeric(quantile(variogram$u, prob = 0.1)),
+                                      as.numeric(quantile(variogram$u, prob = 0.8)),l = 10)
+                                ),
+                                messages = F)
+  return(vario_model = vario_model)
+}
