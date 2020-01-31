8bde415a365ab16b15d3459ee861481a3e7ea101
mgenu <mathieu.genu@univ-lr.fr>
2020-01-31 09:51:18 +0100

    update needed package

--- R/import_package.R
+++ R/import_package.R
@@ -1,14 +1,15 @@
-#' @import rgdal maptools sp foreign FactoMineR missMDA
-#' @import dplyr ggplot2 cowplot mvtnorm Distance captioner Amelia
-#' @import Rdistance dsm knitr fields broom coda
+#' @import rgdal maptools  sp foreign
+#' @import dplyr ggplot2 cowplot mvtnorm Distance captioner
+#' @import Rdistance dsm knitr fields broom
+#' @import purrr VIM Amelia missMDA FactoMineR rstan Rcpp
+#' @import geoR WhatIf viridisLite
 #' @rawNamespace import(ggthemes, except = theme_map)
-#' @rawNamespace import(raster, except = c(intersect, union, select))
+#' @rawNamespace import(raster, except = c(intersect, union, select, extract))
 #' @rawNamespace import(reshape, except = c(stamp, rename))
-# @import methods Rcpp
-#' @importFrom stats integrate rnorm sd as.formula coef cor diffinv glm
-#' poisson rgamma rnbinom rpois var vcov
+#' @rawNamespace import(coda, except = c(traceplot))
+#' @rawNamespace import(arm, except = c(traceplot))
+#' @rawNamespace import(MASS, except = c(area, select))
+#' @importFrom stats integrate rnorm sd as.formula coef cor diffinv glm poisson rgamma rnbinom rpois var vcov
 #' @importFrom lubridate ymd
-# #' @importFrom rstan sampling
-# #' @useDynLib  geffaeR,  .registration = TRUE
 #' @importFrom utils combn
 NULL
